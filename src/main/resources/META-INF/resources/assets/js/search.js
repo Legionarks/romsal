55fbcf297675afd3ca291cc3bb346f4f23ed02ad
Legionarks <jorge.michelen@hotmail.com>
2021-07-21 23:29:01 -0400

    Improve designs

--- src/main/resources/META-INF/resources/assets/js/search.js
+++ src/main/resources/META-INF/resources/assets/js/search.js
@@ -1,4 +1,9 @@
 $(function () {
+  filter();
+  fill();
+});
+
+function filter() {
   const min = parseInt($("input[name=price-min]").val());
   const max = parseInt($("input[name=price-max]").val());
   const rates = parseFloat($("#slider-range").attr("rates"));
@@ -41,14 +46,40 @@ $(function () {
   });
 
   $("#amount").text($("input[name=currency]:checked").val() + "$" + $("#slider-range").slider("values", 0) + " - " + $("input[name=currency]:checked").val() + "$" + $("#slider-range").slider("values", 1));
-});
+}
+
+function fill() {
+  const params = (new URL(document.location)).searchParams;
+
+  $("#address").val(params.get("address"));
+  $("#type").val(params.get("type"));
+  $("#room").val("")
+  $("#category").val(params.get("category"));
+  $("#room").val(params.get("room"));
+  $("#bath").val(params.get("bath"));
+
+  if (params.has("currency")) {
+    $("input[name=currency][value=" + params.get("currency") + "]").click();
+
+    if (params.has("price-min") && params.has("price-max")) {
+      $("#price-min").val(params.get("price-min"));
+      $("#price-max").val(params.get("price-max"));
+      $("#slider-range").slider("values", 0, params.get("price-min"));
+      $("#slider-range").slider("values", 1, params.get("price-max"));
+      $("#amount").text($("input[name=currency]:checked").val() + "$" + params.get("price-min") + " - " + $("input[name=currency]:checked").val() + "$" + params.get("price-max"));
+    }
+  }
+}
 
 $("#clean").click(function () {
   $("#address").val("");
   $("#type").val($("#type option:first").val());
-  $("#bath").val("");
   $("#room").val("");
+  $("#bath").val("");
   $("#category").val($("#category option:first").val());
+  $("input[name=currency][value=USD]").click();
+  $("#price-min").val($("#slider-range").slider("option", "min"));
+  $("#price-max").val($("#slider-range").slider("option", "max"));
   $("#slider-range").slider("values", 0, $("#slider-range").slider("option", "min"));
   $("#slider-range").slider("values", 1, $("#slider-range").slider("option", "max"));
   $("#amount").text($("input[name=currency]:checked").val() + "$" + $("#slider-range").slider("values", 0) + " - " + $("input[name=currency]:checked").val() + "$" + $("#slider-range").slider("values", 1));
