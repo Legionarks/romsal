be63201d6e349ac74829e7960cc1fa8d597aa6be
Legionarks <jorge.michelen@hotmail.com>
2021-09-08 23:10:24 -0400

    Create new structure
    Restruturate project based on gradle's multi-build architecture
    - No original files have been included yet

---
+++ admin-application/src/native-test/java/com/github/legionarks/NativeTestIT.java
@@ -0,0 +1,9 @@
+package com.github.legionarks;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeTestIT extends TestTest {
+
+    // Execute the same tests but in native mode.
+}
